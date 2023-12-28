97e6234722beb478d6c2d2e73cfe10f08c9801e6
Jason Ridgway-Taylor <jason@ridgway-taylor.co.uk>
2023-12-28 13:44:54 +0700

    Added skeleton console app

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "TradingView hook to IG trade order";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs;[
+  git
+  fsharp
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.dotnet.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
